f31baedd20a9f6c7bee0b9586ceb9b1e19fb092a
wangleiyong <wangleiyong@jd.com>
2018-09-27 15:49:37 +0800

    修改网络框架

--- libcore/src/main/java/com/wlazy/core/http/BaseRequest.java
+++ libcore/src/main/java/com/wlazy/core/http/BaseRequest.java
@@ -2,6 +2,7 @@ package com.wlazy.core.http;
 
 import android.support.annotation.Nullable;
 
+import com.google.gson.Gson;
 import com.wlazy.core.flowable.Transformers;
 import com.wlazy.core.http.service.IBaseService;
 
@@ -11,6 +12,7 @@ import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 
@@ -19,17 +21,38 @@ public abstract class BaseRequest<T> implements Function<ResponseBody, T> {
     private IBaseService baseService;
 
     public BaseRequest(String baseUrl) {
-        baseService = new RetrofitClient.Builder().hostUrl(baseUrl).build().createNetService(IBaseService.class);
+        baseService = buildClient(baseUrl).createNetService(IBaseService.class);
     }
 
-    public Flowable<T> reqPost(final String func, @Nullable final Map<String, String> param) {
+    protected RetrofitClient buildClient(String baseUrl){
+        //可以重载此方法 修改相关参数
+        return new RetrofitClient.Builder().hostUrl(baseUrl).build();
+    }
+
+    public Flowable reqPost(final String func, @Nullable final Map<String, String> params) {
         if(this.baseService == null) {
             throw new IllegalStateException("未初始化IBaseService");
         } else {
-            return this.baseService.postFunc(func, buildParms(param)).map(this).subscribeOn(Schedulers.io()).compose(Transformers.schedulersTransformer());
+            return this.baseService.postFunc(func, buildRequestBody(params)).map(this).compose(Transformers.schedulersTransformer());
         }
     }
 
-    abstract protected RequestBody buildParms(@Nullable final Map<String, String> param);
+    public <K> Flowable reqPost(final String func, K h) {
+        if(this.baseService == null) {
+            throw new IllegalStateException("未初始化IBaseService");
+        } else {
+            return this.baseService.postFunc(func, buildRequestBody(new Gson().toJson(h))).map(this).compose(Transformers.schedulersTransformer());
+        }
+    }
+
+    protected RequestBody buildRequestBody(@Nullable final Map<String, String> params){
+        //此处可以加一些公共参数
+        return buildRequestBody(new Gson().toJson(params));
+    }
+
+    protected RequestBody buildRequestBody(@Nullable final String json){
+        //此处可以加一些公共参数
+        return RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
+    }
 
 }
